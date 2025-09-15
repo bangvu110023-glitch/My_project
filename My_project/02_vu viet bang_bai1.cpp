a5cd552ed909abb80491308cee65ffeb846f0614
bangvu2k4 <bangvu110023@gmail.com>
2025-09-15 22:26:07 +0700

    Add files via upload

---
+++ My_project/02_vu viet bang_bai1.cpp
@@ -0,0 +1,31 @@
+#include "DHT.h"
+
+#define DHTPIN 4
+#define DHTTYPE DHT11
+
+DHT dht(DHTPIN, DHTTYPE);
+
+void setup() {
+  Serial.begin(115200);
+  dht.begin();
+  Serial.println("Bat dau doc DHT11...");
+}
+
+void loop() {
+  float h = dht.readHumidity();
+  float t = dht.readTemperature();
+
+  if (isnan(h) || isnan(t)) {
+    Serial.println("Loi khi doc DHT11!");
+  } else {
+    Serial.print("Nhiet do: ");
+    Serial.print(t);
+    Serial.println(" *C");
+
+    Serial.print("Do am: ");
+    Serial.print(h);
+    Serial.println(" %");
+  }
+
+  delay(2000);
+}
